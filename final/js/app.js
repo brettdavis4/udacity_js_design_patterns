24804c54b20b3e80810d0ec59729bce7261d1863
Brett Davis <brettdavis4@gmail.com>
2016-04-06 13:43:34 -0400

    Update app.js

--- final/js/app.js
+++ final/js/app.js
@@ -1,3 +1,14 @@
+//yelp authorization keys
+var auth = {
+    consumerKey: "-BniUiKB-N6v3CvXd-zTsg",
+    consumerSecret: "d7bxrnwhdgjuHW9Eu5Uz0gxoREo",
+    accessToken: "JbTFDVc1uq7W1MyrBQHpfvGFkH13OFuO",
+    accessTokenSecret: "xC7re40MACooIPlfnIAPveYnrxw",
+    serviceProvider: {
+        signatureMethod: "HMAC-SHA1"
+    }
+};
+
 //array of places that sell burgers around downtown Indy
 var initialBurgerJoints = [
     {
