f078418b2832cbe0408131d73267cb9545905652
Brett Davis <brettdavis4@gmail.com>
2016-08-01 10:09:28 -0400

    Update googlemap.js

--- final/js/googlemap.js
+++ final/js/googlemap.js
@@ -46,7 +46,7 @@ function addmarker(lat,lng, id, name, comments){
     parameters.push(['oauth_token', auth.accessToken]);
     parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
     var message = {
-        'action': 'http://api.yelp.com/v2/search',
+        'action': 'https://api.yelp.com/v2/search',
         'method': 'GET',
         'parameters': parameters
     };
